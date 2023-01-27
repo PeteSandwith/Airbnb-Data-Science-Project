155acefd754b6a9ddfd06a5774b98d557e2c6a8a
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-01-27 15:54:24 +0000

    Packages all of the code that cleans the dataframe into a single function called clean_tabular_data

--- tabular_data.py
+++ tabular_data.py
@@ -5,7 +5,11 @@ import ast
 
 dataset = pd.read_csv('airbnb-property-listings/tabular_data/listing.csv')
 
-
+def clean_tabular_data(dataframe):
+    dataframe = set_default_feature_values(dataframe)
+    dataframe = remove_rows_with_missing_ratings(dataframe)
+    dataframe = combine_description_strings(dataframe)
+    return(dataframe)
 
 # Function that sets missing values in the 'beds', 'guests' 'bathrooms' and 'bedrooms' columns to 1 and returns the updated dataframe.
 def set_default_feature_values(dataframe):
@@ -13,7 +17,7 @@ def set_default_feature_values(dataframe):
     dataframe['guests'].where(~dataframe['guests'].isna(), 1, inplace = True)
     dataframe['bathrooms'].where(~dataframe['bathrooms'].isna(), 1, inplace = True)
     dataframe['bedrooms'].where(~dataframe['bedrooms'].isna(), 1, inplace = True)
-    dataframe['Description'].where(~dataframe['Description'].isna(), '["No description given"]', inplace = True)
+    dataframe['Description'].where(~dataframe['Description'].isna(), '["About this space", "No", "description given"]', inplace = True)
     return dataframe
 
 # Function that removes any rows that have missing values in the ratings columns
@@ -21,29 +25,28 @@ def remove_rows_with_missing_ratings(dataframe):
     dataframe.dropna(axis = 0, how = 'any', subset = ['Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating'], inplace = True)
     return dataframe
 
-
+def combine_description_strings(dataframe):
+    dataframe['Description'] = dataframe['Description'].apply(parses_description_strings)
+    return dataframe
 # Function that parses the string-valued entries of the description column, which are strings whose contents are valid lists, and converts them to orderly strings with no erroneous extra spaces.
 def parses_description_strings(string):
-    #Uses ast package to parse the string description into a list
-    description_as_list = ast.literal_eval(string)
-    description_as_list.pop(0)
-    #Removes empty quotes from the list
-    number_empty_quotes = description_as_list.count('')
-    for index in range(0,number_empty_quotes):
-        description_as_list.remove('')
-    #Turns the list into a single string containing the description
-    cleaned_description = ' '.join(description_as_list)
-    return cleaned_description
+    try:
+        #Uses ast package to parse the string description into a list
+        description_as_list = ast.literal_eval(string)
+        description_as_list.pop(0)
+        #Removes empty quotes from the list
+        number_empty_quotes = description_as_list.count('')
+        for index in range(0,number_empty_quotes):
+            description_as_list.remove('')
+        #Turns the list into a single string containing the description
+        cleaned_description = ' '.join(description_as_list)
+        return cleaned_description
+    except: 
+        return string
 
+dataset = clean_tabular_data(dataset)
+print(dataset.head())
 
-
-
-
-dataset = set_default_feature_values(dataset)
-dataset = remove_rows_with_missing_ratings(dataset)
-#dataset = combine_description_strings(dataset)
-#print(dataset.count())
-print(dataset.head(20))
 
 
 
