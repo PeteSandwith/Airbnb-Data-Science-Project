7eff92cc13c6247003e2cacf9397ca17bfe84b66
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-01-27 13:09:29 +0000

    Creates set_default_feature_values column that replaces missing values in certain columns with a 1

--- tabular_data.py
+++ tabular_data.py
@@ -1,6 +1,24 @@
+# %%
 import pandas as pd
 
 dataset = pd.read_csv('airbnb-property-listings/tabular_data/listing.csv')
 
 
-print(dataset.head())
+
+# Function that sets missing values in the 'beds', 'guests' 'bathrooms' and 'bedrooms' columns to 1 and returns the updated dataframe.
+def set_default_feature_values(dataframe):
+    dataframe['beds'] = dataframe['beds'].where(~dataframe['beds'].isna(), 1)
+    dataframe['guests'] = dataframe['guests'].where(~dataframe['guests'].isna(), 1)
+    dataframe['bathrooms'] = dataframe['bathrooms'].where(~dataframe['bathrooms'].isna(), 1)
+    dataframe['bedrooms'] = dataframe['bedrooms'].where(~dataframe['bedrooms'].isna(), 1)
+    return dataframe
+
+
+
+dataset = set_default_feature_values(dataset)
+print(dataset.head(20))
+
+#'Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating'
+
+
+# %%
