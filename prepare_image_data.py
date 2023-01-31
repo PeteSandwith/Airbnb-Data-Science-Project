88ed4a7c7227f5010894586f24af61e11974d0b7
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-01-31 16:37:26 +0000

    Creates a function to save a processed image in a specific directory

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,17 +1,13 @@
 
 import cv2 as cv
 import pandas as pd
+import os
 # Function has to :
 # 1. Load an image from Airbnb-Data-Science-Project/airbnb_property_listings/images 
 # 2. Resizes it to the same height and width; set the height of the smallest image as the height for all of the other images.
 #    Your code should maintain the aspect ratio of the image and adjust the width proportionally to the change in height, rather than just squashing it vertically.
 # 3. Check that the image is in RBG format; if it isn't it should be discarded.
 # 4. Save the processed image in the folder Airbnb-Data-Science-Project/airbnb_property_listings/processed_images
-
-
-
-
-
 
 
 
@@ -28,13 +24,24 @@ def resize_image(image):
     image = cv.resize(image, (0,0), fx = scale, fy = scale)
     return image
 
+#The following function saves an image in the new processed images folder
+def save_image(image, id, letter):
+    processed_image_location = 'airbnb-property-listings/processed_images/{}'.format(id) + '/{}'.format(id) + '-{}.png'.format(letter)
+    print(processed_image_location)
+    Save = cv.imwrite(processed_image_location, image)
+    print(Save)
+
+
 
 id = 'f9dcbd09-32ac-41d9-a0b1-fdb2793378cf'
+#Creates a directory inside processed_images where all images of the same uuid will be saved
+os.mkdir('airbnb-property-listings/processed_images/{}'.format(id))
 letters = ['a', 'b', 'c', 'd', 'e']
 for letter in letters:
+    
     image = load_image(id, letter)
     image = resize_image(image)
-    
+    save_image(image, id = id, letter = letter)
 
 
 #cv.imshow('Image', image)
