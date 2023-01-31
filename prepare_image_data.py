30c34f8e9042fb94f7d3c987017a57fe6a655130
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-01-31 12:59:33 +0000

    Imports opencv package for image processing

---
+++ prepare_image_data.py
@@ -0,0 +1,14 @@
+# %%
+import cv2
+
+# Function has to :
+# 1. Load an image from Airbnb-Data-Science-Project/airbnb_property_listings/images 
+# 2. Resizes it to the same height and width; set the height of the smallest image as the height for all of the other images.
+#    Your code should maintain the aspect ratio of the image and adjust the width proportionally to the change in height, rather than just squashing it vertically.
+# 3. Check that the image is in RBG format; if it isn't it should be discarded.
+# 4. Save the processed image in the folder Airbnb-Data-Science-Project/airbnb_property_listings/processed_images
+
+
+
+
+# %%
