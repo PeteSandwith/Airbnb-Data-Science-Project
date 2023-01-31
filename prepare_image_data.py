3e2ced534fe1ab838ca50e68bd47f0ad14412195
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-01-31 14:27:35 +0000

    Loads a single image

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,6 +1,5 @@
-# %%
-import cv2
 
+import cv2 as cv
 # Function has to :
 # 1. Load an image from Airbnb-Data-Science-Project/airbnb_property_listings/images 
 # 2. Resizes it to the same height and width; set the height of the smallest image as the height for all of the other images.
@@ -8,7 +7,11 @@ import cv2
 # 3. Check that the image is in RBG format; if it isn't it should be discarded.
 # 4. Save the processed image in the folder Airbnb-Data-Science-Project/airbnb_property_listings/processed_images
 
+image = cv.imread('airbnb-property-listings/images/0a26e526-1adf-4a2a-888d-a05f7f0a2f33/0a26e526-1adf-4a2a-888d-a05f7f0a2f33-a.png', cv.IMREAD_COLOR)
+#cv.imshow('Image', image)
+#cv.waitKey(0)
+#cv.destroyAllWindows()
 
 
 
-# %%
+
