648b63b15d3ffe7735d5cc1f1983e02e5536ab5e
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-02-02 10:34:46 +0000

    Trains the model and calculates the mean squared error on the test set

--- modelling.py
+++ modelling.py
@@ -2,9 +2,14 @@ import sklearn
 from sklearn.model_selection import train_test_split
 from tabular_data import load_airbnb
 from sklearn import linear_model
+from sklearn import metrics
 
 X, y = load_airbnb('cleaned_tabular_data.csv')
-
+#Splits the data into training and testing sets.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
+#Creates and trains the model
 model = linear_model.SGDRegressor()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print(metrics.mean_squared_error(y_pred, y_test))
