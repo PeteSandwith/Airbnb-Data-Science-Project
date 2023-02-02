c8cb9d8bb935e93bb054d272488221219d345b46
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-02-02 09:30:55 +0000

    Imports linear_model package, creates SGDRegressor model

--- modelling.py
+++ modelling.py
@@ -1,7 +1,10 @@
 import sklearn
 from sklearn.model_selection import train_test_split
 from tabular_data import load_airbnb
+from sklearn import linear_model
 
 X, y = load_airbnb('cleaned_tabular_data.csv')
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+model = linear_model.SGDRegressor()
