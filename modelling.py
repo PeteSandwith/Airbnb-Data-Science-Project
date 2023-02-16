9af807be5b2a4f2e13b427ccbaf3368cb2fbf507
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-02-16 12:22:17 +0000

    Adds explanations to code

--- modelling.py
+++ modelling.py
@@ -41,15 +41,18 @@ def calculate_rmse():
 
 hyperparameters = {'loss': ['epsilon_insensitive', 'squared_error', 'squared_epsilon_insensitive', 'huber'], 'penalty': ['l2', 'l1', 'elasticnet'], 'alpha': [0.00006, 0.00008, 0.0001, 0.00012, 0.00014, 0.00015,  0.00016, 0.00018], 'max_iter': [3000]}
 
+# Custom grid search function 
 def custom_tune_regression_model_hyperparameters(model_class, dataset, hyperparameters):
     hyperparameter_combinations = []
     best_model = {'model': 0, 'validation_RMSE': -10000}
 
+    # Fills hyperparameter_combinations with dicitonaries corresponding to all possible combinations of the hyperparameters in the ranges specified.
     for loss in hyperparameters['loss']:
         for penalty in hyperparameters['penalty']:
             for alpha in hyperparameters['alpha']:
                 hyperparameter_combinations.append({'loss': loss, 'penalty': penalty, 'alpha': alpha})
 
+    # Trains and evaluates a model for each combination of hyperparameters and determines the best model.
     for combination in hyperparameter_combinations:
         model = model_class(loss = combination['loss'], penalty = combination['penalty'], alpha = combination['alpha'], max_iter= 3000)
         model.fit(dataset['X_train'], dataset['y_train'])
@@ -61,11 +64,12 @@ def custom_tune_regression_model_hyperparameters(model_class, dataset, hyperpara
 
     return best_model
 
+# Uses GridSearchCV to determine the best comibination of hyperparameters and returns the best performing model. 
 def tune_regression_model_hyperparameters(hyperparameters):
     grid = sklearn.model_selection.GridSearchCV(estimator= linear_model.SGDRegressor(), param_grid= hyperparameters, scoring= 'r2', refit= 'r2', verbose= 10)
     grid.fit(X_train, y_train)
-    print(grid.best_estimator_)
-    print(grid.best_params_)
+    #print(grid.best_estimator_)
+    #print(grid.best_params_)
     best_estimator = grid.best_estimator_
     return best_estimator
 
@@ -73,6 +77,8 @@ def tune_regression_model_hyperparameters(hyperparameters):
 
 best_estimator = tune_regression_model_hyperparameters(hyperparameters= hyperparameters)
 y_predict = best_estimator.predict(X_validation)
+
 print(metrics.r2_score(y_validation, y_predict))
 calculate_R2()
+
 #print(custom_tune_regression_model_hyperparameters(model_class = linear_model.SGDRegressor, dataset={'X_train': X_train, 'y_train': y_train, 'X_validation': X_validation, 'y_validation': y_validation}, hyperparameters= hyperparameters))
