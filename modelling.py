3206c7df98e5f25d726b72a84c1d41b44de5fd2a
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-02-20 12:31:17 +0000

    Creates a class for the base SGDRegression model

--- modelling.py
+++ modelling.py
@@ -20,42 +20,43 @@ def prepare_data(data):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state= 2)
     X_validation, X_test, y_validation, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state= 2)
     return X_train, X_validation, X_test, y_train, y_validation, y_test
+
+class base_regression_model:
+    def __init__(self):
+        y_predictions_train, y_predictions_test, y_predictions_validation = self.__train_and_predict__()
+        self.__calculate_R2__(y_predictions_train = y_predictions_train, y_predictions_test = y_predictions_test, y_predictions_validation = y_predictions_validation)
+        self.__calculate_rmse__(y_predictions_train = y_predictions_train, y_predictions_test = y_predictions_test, y_predictions_validation = y_predictions_validation)
+
     
-class base_regression_model():
-    y_predictions_train, y_predictions_test, y_predictions_validation = train_and_predict()
-    calculate_R2(y_predictions_train = y_predictions_train, y_predictions_test = y_predictions_test, y_predictions_validation = y_predictions_validation)
-    calculate_rmse(y_predictions_train = y_predictions_train, y_predictions_test = y_predictions_test, y_predictions_validation = y_predictions_validation)
+    def __train_and_predict__(self):
+        # Creates and trains the model
+        model = linear_model.SGDRegressor()
+        model.fit(X_train, y_train)
+        
+        #Makes predictions
+        y_predictions_train = model.predict(X_train)
+        y_predictions_test = model.predict(X_test)
+        y_predictions_validation = model.predict(X_validation)
 
-# Generates label predictions for the training and test sets
-def train_and_predict():
-    # Creates and trains the model
-    model = linear_model.SGDRegressor()
-    model.fit(X_train, y_train)
-    
-    #Makes predictions
-    y_predictions_train = model.predict(X_train)
-    y_predictions_test = model.predict(X_test)
-    y_predictions_validation = model.predict(X_validation)
+        return y_predictions_train, y_predictions_test, y_predictions_validation
 
-    return y_predictions_train, y_predictions_test, y_predictions_validation
+    # Calculates the R^2 regression score function for training, test and validation sets
+    def __calculate_R2__(self, y_predictions_train, y_predictions_test, y_predictions_validation):
+        R2_train = metrics.r2_score(y_train, y_predictions_train)
+        R2_test = metrics.r2_score(y_test, y_predictions_test)
+        R2_validation = metrics.r2_score(y_validation, y_predictions_validation)
+        print('The R2 score for the training set is: ' + str(R2_train))
+        print('The R2 score for the test set is: ' + str(R2_test))
+        print('The R2 score for the validation set is: ' + str(R2_validation))
 
-# Calculates the R^2 regression score function for training, test and validation sets
-def calculate_R2(y_predictions_train, y_predictions_test, y_predictions_validation):
-    R2_train = metrics.r2_score(y_train, y_predictions_train)
-    R2_test = metrics.r2_score(y_test, y_predictions_test)
-    R2_validation = metrics.r2_score(y_validation, y_predictions_validation)
-    print('The R2 score for the training set is: ' + str(R2_train))
-    print('The R2 score for the test set is: ' + str(R2_test))
-    print('The R2 score for the validation set is: ' + str(R2_validation))
-
-# Calculates the root mean squared error for training, test and validation sets
-def calculate_rmse(y_predictions_train, y_predictions_test, y_predictions_validation):
-    rmse_train = metrics.mean_squared_error(y_train, y_predictions_train, squared = False)
-    rmse_test = metrics.mean_squared_error(y_test, y_predictions_test, squared = False)
-    rmse_validation = metrics.mean_squared_error(y_validation, y_predictions_validation, squared = False)
-    print('The rmse for the training set is: ' + str(rmse_train))
-    print('The rmse for the test set is: ' + str(rmse_test))
-    print('The rmse for the test set is: ' + str(rmse_validation))
+    # Calculates the root mean squared error for training, test and validation sets
+    def __calculate_rmse__(self, y_predictions_train, y_predictions_test, y_predictions_validation):
+        rmse_train = metrics.mean_squared_error(y_train, y_predictions_train, squared = False)
+        rmse_test = metrics.mean_squared_error(y_test, y_predictions_test, squared = False)
+        rmse_validation = metrics.mean_squared_error(y_validation, y_predictions_validation, squared = False)
+        print('The rmse for the training set is: ' + str(rmse_train))
+        print('The rmse for the test set is: ' + str(rmse_test))
+        print('The rmse for the test set is: ' + str(rmse_validation))
 
 hyperparameters = {'loss': ['epsilon_insensitive', 'squared_error', 'squared_epsilon_insensitive', 'huber'], 'penalty': ['l2', 'l1', 'elasticnet'], 'alpha': [0.00006, 0.00008, 0.0001, 0.00012, 0.00014, 0.00015,  0.00016, 0.00018], 'max_iter': [3000]}
 
@@ -122,8 +123,9 @@ def save_model(folder, model, metrics, hyperparameters):
 
 
 X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_data(data= 'cleaned_tabular_data.csv')
+base_regression_model()
 
-best_estimator, best_performance_metrics, best_hyperparameters = tune_regression_model_hyperparameters(model = linear_model.SGDRegressor(), hyperparameters= hyperparameters)
+#best_estimator, best_performance_metrics, best_hyperparameters = tune_regression_model_hyperparameters(model = linear_model.SGDRegressor(), hyperparameters= hyperparameters)
 
 #save_model(folder = 'Models/Regression/Linear_Regression/', model = best_estimator, metrics = best_performance_metrics, hyperparameters = best_hyperparameters)
 
