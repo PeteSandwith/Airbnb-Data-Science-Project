795cad52e59bfe7538e2c77904842162ee5c45bd
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-02-27 17:31:09 +0000

    Creates a basic classification model

--- classification.py
+++ classification.py
@@ -5,3 +5,6 @@ from modelling import prepare_data
 
 X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_data(data = 'cleaned_tabular_data.csv', feature_columns= ['guests', 'beds', 'bathrooms', 'Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating', 'amenities_count', 'bedrooms'], label_columns='Category')
 
+model = sklearn.linear_model.LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_validation, y_validation))
