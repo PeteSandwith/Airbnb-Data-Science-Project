cf5a2fafbf9b080764079dad12cc3513b2757390
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-02-28 10:57:34 +0000

    Creates function to calculate f1 score

--- classification.py
+++ classification.py
@@ -1,10 +1,19 @@
 import sklearn
+from sklearn import metrics
 import numpy as np
 from modelling import prepare_data
 
+
+# Use sklearn to compute the key measures of performance for your classification model. 
+# That should include the F1 score, the precision, the recall, and the accuracy for both the training and test sets.
 
 X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_data(data = 'cleaned_tabular_data.csv', feature_columns= ['guests', 'beds', 'bathrooms', 'Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating', 'amenities_count', 'bedrooms'], label_columns='Category')
 
 model = sklearn.linear_model.LogisticRegression()
 model.fit(X_train, y_train)
 print(model.score(X_validation, y_validation))
+
+def calculate_f1(X, y, model):
+    y_predicted = model.predict(X)
+    f1 = metrics.f1_score(y, y_predicted)
+    return f1
