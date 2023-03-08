97355c622ec0d89f96c7e16e7433c006f18e3132
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-03-08 10:13:44 +0000

    Uses load_airbnb function in init method

--- deeplearning.py
+++ deeplearning.py
@@ -1,14 +1,16 @@
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
-
+from tabular_data import load_airbnb
 
 
 
 class AirbnbDataset(Dataset):
     def __init__(self):
         super().__init__()
-        self.data = pd.read_csv('airbnb-property-listings/tabular_data/cleaned_tabular_data.csv')
+        self.X, self.y = load_airbnb(file= 'cleaned_tabular_data.csv',
+        feature_columns = ['guests', 'beds', 'bathrooms', 'Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating', 'amenities_count', 'bedrooms'],
+        label_columns = 'Price_Night')
    
     def __getitem__(self, index):
         example = self.data.iloc[index]
@@ -19,5 +21,5 @@ class AirbnbDataset(Dataset):
 
 
 data = AirbnbDataset()
-print(data[10])
-print(len(data))
+#print(data[10])
+print(data)
