0f1fcd50e7fd6b9a0d0c0207dd7d373bf944fc27
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-03-08 09:40:32 +0000

    Creates method for indexing pytorch dataset

--- deeplearning.py
+++ deeplearning.py
@@ -10,3 +10,8 @@ class AirbnbDataset(Dataset):
         super().__init__()
         self.data = pd.read_csv('airbnb-property-listings/tabular_data/cleaned_tabular_data.csv')
    
+    def __getitem__(self, index):
+        example = self.data.iloc[index]
+        features = example['guests', 'beds', 'bathrooms', 'Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating', 'amenities_count', 'bedrooms']
+        label = example['Price_Night']
+        return features, label
