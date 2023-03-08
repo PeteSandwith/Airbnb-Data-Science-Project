eda39be8e688989f04b6315b6e7da63dd00b0bec
PeteSandwith <94627453+PeteSandwith@users.noreply.github.com>
2023-03-08 09:35:30 +0000

    Creates pytorch dataset class with init method

--- deeplearning.py
+++ deeplearning.py
@@ -3,3 +3,10 @@ from torch.utils.data import Dataset
 import pandas as pd
 
 data = pd.read_csv('airbnb-property-listings/tabular_data/cleaned_tabular_data.csv')
+
+
+class AirbnbDataset(Dataset):
+    def __init__(self):
+        super().__init__()
+        self.data = pd.read_csv('airbnb-property-listings/tabular_data/cleaned_tabular_data.csv')
+   
